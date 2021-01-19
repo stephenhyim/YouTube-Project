085239bab935c0d90e8a15a6a7e7cbeb7e31b6d9
stephen <stephenhyim@gmail.com>
2021-01-19 17:31:04 -0500

    initial heroku

---
+++ frontend/youtube.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded',() => {
+    const root = document.getElementById('root');
+    // const store = configureStore();
+    ReactDOM.render(<h1>Welcome to YouTube</h1>, root)
+})
