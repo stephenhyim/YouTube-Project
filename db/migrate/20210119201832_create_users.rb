79b5d5e3bd1c5ca5334932ae2c33932eb47c1031
stephen <stephenhyim@gmail.com>
2021-01-20 15:39:38 -0500

    created migration template for user

--- db/migrate/20210119201832_create_users.rb
+++ db/migrate/20210119201832_create_users.rb
@@ -1,7 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :username, null: false
+      t.string :firstname, null: false
+      t.string :lastname, null: false
+      t.integer :phone_number
+      t.date :birthdate, null: false
+      t.string :gender, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
       t.timestamps
     end
   end
