8a3eda73d7b8b4d7a1a33c7b2b70e8f8d9baa500
Stephen Yim <stephenhyim@gmail.com>
2021-05-03 18:16:01 -0400

    getting ready for deployment

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -4738,17 +4738,22 @@ var VideosReducer = function VideosReducer() {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var redux__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! redux */ "./node_modules/redux/es/redux.js");
 /* harmony import */ var redux_thunk__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! redux-thunk */ "./node_modules/redux-thunk/es/index.js");
-/* harmony import */ var redux_logger__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! redux-logger */ "./node_modules/redux-logger/dist/redux-logger.js");
-/* harmony import */ var redux_logger__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(redux_logger__WEBPACK_IMPORTED_MODULE_2__);
-/* harmony import */ var _reducers_root_reducer__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../reducers/root_reducer */ "./frontend/reducers/root_reducer.js");
+/* harmony import */ var _reducers_root_reducer__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../reducers/root_reducer */ "./frontend/reducers/root_reducer.js");
 
 
 
+var middlewares = [redux_thunk__WEBPACK_IMPORTED_MODULE_1__["default"]];
 
+if (true) {
+  var _require = __webpack_require__(/*! redux-logger */ "./node_modules/redux-logger/dist/redux-logger.js"),
+      logger = _require.logger;
+
+  middlewares.push(logger);
+}
 
 var configureStore = function configureStore() {
   var preloadedState = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-  return Object(redux__WEBPACK_IMPORTED_MODULE_0__["createStore"])(_reducers_root_reducer__WEBPACK_IMPORTED_MODULE_3__["default"], preloadedState, Object(redux__WEBPACK_IMPORTED_MODULE_0__["applyMiddleware"])(redux_thunk__WEBPACK_IMPORTED_MODULE_1__["default"], redux_logger__WEBPACK_IMPORTED_MODULE_2___default.a));
+  return Object(redux__WEBPACK_IMPORTED_MODULE_0__["createStore"])(_reducers_root_reducer__WEBPACK_IMPORTED_MODULE_2__["default"], preloadedState, redux__WEBPACK_IMPORTED_MODULE_0__["applyMiddleware"].apply(void 0, middlewares));
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (configureStore);
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,45 +9,45 @@
 require 'open-uri'
 
 User.destroy_all
+Video.destroy_all
 
 guestuser = User.create!(email: 'guest@email.com', password: 'guestpassword', firstname: 'guest', lastname: 'login', gender: 'male', birthdate: '1991-4-20')
 u2 = User.create!(email: 'yimstephen@email.com', password: 'Starwars', firstname: 'Stephen', lastname: 'Yim', gender: 'male', birthdate: '1991-4-20')
 u3 = User.create!(email: 'jiabeixi@email.com', password: 'Starwars', firstname: 'Beixi', lastname: 'Jia', gender: 'female', birthdate: '1994-9-1')
 
-Video.destroy_all
 
 v1 = Video.create!(title: "My Beautiful Dog", description: "Holding my Goldendoodle in NYC", user_id: u3.id)
-v1_file = open("https://s3.amazonaws.com/youtube-seeds/video1.mp4")
+v1_file = URI.open("https://s3.amazonaws.com/youtube-seeds/video1.mp4")
 v1.video.attach(io: v1_file, filename: "video1.mp4")
 
 v2 = Video.create!(title: "Traffic", description: "Typical traffic in NYC", user_id: u3.id)
-v2_file = open("https://s3.amazonaws.com/youtube-seeds/video2.mp4")
+v2_file = URI.open("https://s3.amazonaws.com/youtube-seeds/video2.mp4")
 v2.video.attach(io: v2_file, filename: "video2.mp4")
 
 v3 = Video.create!(title: "Underwater", description: "Animals swimming underwater", user_id: u2.id)
-v3_file = open("https://s3.amazonaws.com/youtube-seeds/video3.mp4")
+v3_file = URI.open("https://s3.amazonaws.com/youtube-seeds/video3.mp4")
 v3.video.attach(io: v3_file, filename: "video3.mp4")
 
 v4 = Video.create!(title: "Fire", description: "Warm campfire on the beach", user_id: u2.id)
-v4_file = open("https://s3.amazonaws.com/youtube-seeds/video4.mp4")
+v4_file = URI.open("https://s3.amazonaws.com/youtube-seeds/video4.mp4")
 v4.video.attach(io: v4_file, filename: "video4.mp4")
 
 v5 = Video.create!(title: "Mountains", description: "Beautiful moutain landscape", user_id: u2.id)
-v5_file = open("https://s3.amazonaws.com/youtube-seeds/video5.mp4")
+v5_file = URI.open("https://s3.amazonaws.com/youtube-seeds/video5.mp4")
 v5.video.attach(io: v5_file, filename: "video5.mp4")
 
 v6 = Video.create!(title: "Sand", description: "My feet entering the warm sand", user_id: u3.id)
-v6_file = open("https://s3.amazonaws.com/youtube-seeds/video6.mp4")
+v6_file = URI.open("https://s3.amazonaws.com/youtube-seeds/video6.mp4")
 v6.video.attach(io: v6_file, filename: "video6.mp4")
 
 v7 = Video.create!(title: "Coffee", description: "Must needed coffee in the morning", user_id: u2.id)
-v7_file = open("https://s3.amazonaws.com/youtube-seeds/video7.mp4")
+v7_file = URI.open("https://s3.amazonaws.com/youtube-seeds/video7.mp4")
 v7.video.attach(io: v7_file, filename: "video7.mp4")
 
 v8 = Video.create!(title: "Flight", description: "Get ready for takeoff", user_id: u2.id)
-v8_file = open("https://s3.amazonaws.com/youtube-seeds/video8.mp4")
+v8_file = URI.open("https://s3.amazonaws.com/youtube-seeds/video8.mp4")
 v8.video.attach(io: v8_file, filename: "video8.mp4")
 
 v9 = Video.create!(title: "Beach", description: "Watching the waves crash into the shore", user_id: u3.id)
-v9_file = open("https://s3.amazonaws.com/youtube-seeds/video9.mp4")
+v9_file = URI.open("https://s3.amazonaws.com/youtube-seeds/video9.mp4")
 v9.video.attach(io: v9_file, filename: "video9.mp4")
